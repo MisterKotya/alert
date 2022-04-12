d50075d79abd5e9262cd5658135441cbae3999df
Dima Chuprov <chuprov555@mail.ru>
2022-04-12 16:23:43 +0300

    one_lessons

--- alert.js
+++ alert.js
@@ -6,7 +6,7 @@ alert("Привет");
 alert("Мир");
 
 let user1 = "John";
-let age = 25;
+let age23 = 25;
 let message12 = "Hello";
 message12 = "Hellow";
 let myVelyLong = "Vityaz";
@@ -20,7 +20,7 @@ alert("age");
 
 
 
-let admin, name, user23;
+let admin, name23, user23;
 user23 = "John";
 admin = user23;
 alert( admin );  
@@ -42,9 +42,9 @@ let ourPlanetName = "Земля";
 let currentUserName = "Джон";
 let mainUserName = "Мария";
 
-const BIRTHDAY = '18.04.1982'; 
+/*const BIRTHDAY = '18.04.1982'; 
 
-const AGE = first(BIRTHDAY);
+const AGE = first(BIRTHDAY); */
 
 /*Типы данных*/
 /*Динамическая типизация - типы данных есть, но переменные не привязаны ни к одному из них*/
@@ -52,9 +52,64 @@ let message = "12345";
 message = "User";
 
 /*Число*/
-
 let n = "12345";
 n = "12.345";
+alert (1/0); // (1/0 = Infinity) - бесконечность
+alert ("не число"/ 2+5); //Nan - мат ошибка
+/*const bigInt = 1234567890123456789012345678901234567890n;  bigInt - это тип, который дает прописывать огроменные числа с n в конце*/
 
-alert (1/0); // Infinity - бесконечность
+
+/*Строка*/
+/*let str0 = "Привет";
+let str1 = `${str}`; //Обратные кавычки позволяют встраивать переменные `${str}`;*/
+let name123 = "Иван";
+alert ( `Привет, ${name123}!` );
+alert (`результат: ${1+2}`);
+alert (`вывод: ${250+250}`);
+
+/*Булевый логический тип*/
+//Булевый тип (boolean) может принимать только два значения: true (истина) и false (ложь).
+let isGreater = 5 > 1;
+alert(isGreater); // true - результатом сравнения будет да
+
+/*Спец значение null - представляет собой «ничего», «пусто» или «значение неизвестно»*/
+let age1 = null; // Значение неизвестно
+
+/*Специальное значение undefined - значение не было присвоено; undefined – для проверок, была ли переменная назначена.*/
+let ag1e = 123;
+ag1e = undefined;
+alert (ag1e);
+
+/*Оператор typeof позволяет нам увидеть, какой тип данных сохранён в переменной.
+Имеет две формы: typeof x или typeof(x).*/
+
+let name = "Ilya";
+
+alert(`hello ${1}`);
+alert(`hello ${"name"}`);
+alert (`hello ${name}`);
+
+/*Взаимодействие: alert, prompt, confirm*/
+/*Функция prompt принимает два аргумента:
+title - Текст для отображения в окне.
+default - Необязательный второй параметр, 
+который устанавливает начальное значение в поле для текста в окне.
+result = prompt(title, [default]);*/
+let age = prompt(`Сколько тебе лет?`, 100);
+alert (`Тебе ${age}лет!`); // Тебе 100 лет! 
+
+/*Функция confirm отображает модальное окно с текстом вопроса question и двумя кнопками: OK и Отмена.
+Результат – true, если нажата кнопка OK. В других случаях – false.
+result = confirm(question);*/
+let isBoss = confirm("Ты здесь главный?");
+alert(isBoss); //true - если нажата ОК
+
+
+let name1231 = prompt("Ваше имя?", "");
+alert(name1231);
+
+
+
+
+
 
