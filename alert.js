cf6b9731d1933e639670a18eb9a8a7aa2c8a7acf
Dima Chuprov <chuprov555@mail.ru>
2022-04-12 14:34:48 +0300

    Begin

---
+++ alert.js
@@ -0,0 +1,60 @@
+// Этот код используется в строгом режиме
+"use strict";
+
+alert("Привет");
+
+alert("Мир");
+
+let user1 = "John";
+let age = 25;
+let message12 = "Hello";
+message12 = "Hellow";
+let myVelyLong = "Vityaz";
+let user123 = "123";
+let _ = "3";
+let $ = "4";
+alert("_+$"); //7
+
+alert("age"); 
+
+
+
+
+let admin, name, user23;
+user23 = "John";
+admin = user23;
+alert( admin );  
+
+
+/*const myBirthday = "16.01.1986"; - правильное
+myBirthday = "19.02.2090"; - неправильное*/
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...когда нам нужно выбрать цвет
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+let ourPlanetName = "Земля";
+let currentUserName = "Джон";
+let mainUserName = "Мария";
+
+const BIRTHDAY = '18.04.1982'; 
+
+const AGE = first(BIRTHDAY);
+
+/*Типы данных*/
+/*Динамическая типизация - типы данных есть, но переменные не привязаны ни к одному из них*/
+let message = "12345";
+message = "User";
+
+/*Число*/
+
+let n = "12345";
+n = "12.345";
+
+alert (1/0); // Infinity - бесконечность
+
